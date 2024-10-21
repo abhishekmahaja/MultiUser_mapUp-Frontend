e4239aa21d65d6b4093d28bde706712ca2f46637
abhishekmahajan <abhishekmahajan8285@gmail.com>
2024-10-21 17:55:01 +0530

    localhost

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -1,7 +1,7 @@
 //for live server
-export const USER_API = "https://rtms-backend.onrender.com/api/v1/users";
-export const ORGANIZATION_API = "https://rtms-backend.onrender.com/api/v1/organization";
+// export const USER_API = "https://rtms-backend.onrender.com/api/v1/users";
+// export const ORGANIZATION_API = "https://rtms-backend.onrender.com/api/v1/organization";
 
 //for localhost server
-// export const USER_API = "http://localhost:5000/api/v1/users";
-// export const ORGANIZATION_API = "http://localhost:5000/api/v1/organization"
+export const USER_API = "http://localhost:5000/api/v1/users";
+export const ORGANIZATION_API = "http://localhost:5000/api/v1/organization"
