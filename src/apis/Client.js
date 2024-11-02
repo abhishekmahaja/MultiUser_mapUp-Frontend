b60ba7104ecb36b472e7891073290e83b51a27e7
Abhi Mahajan <52094132+abhishekmahaja@users.noreply.github.com>
2024-11-02 22:58:58 +0530

    Update Client.js

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -4,6 +4,6 @@
 // export const External_API = "https://rtms-backend.onrender.com/api/v1/externaldevice"
 
 //for localhost server
-export const USER_API = "http://localhost:5000/api/v1/users";
-export const ORGANIZATION_API = "http://localhost:5000/api/v1/organization"
-export const External_API = "http://localhost:5000/api/v1/externaldevice"
+export const USER_API = "http://localhost:7000/api/v1/users";
+export const ORGANIZATION_API = "http://localhost:7000/api/v1/organization"
+export const External_API = "http://localhost:7000/api/v1/externaldevice"
