d1e54c49655b1aa4b738744e33eadbf578935e19
abhishekmahajan <abhishekmahajan8285@gmail.com>
2024-11-02 18:06:24 +0530

    Edf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB (1 MB)
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
